0f9b1793a2d147952ff053a19c593b5a4968144c
mqy <mqy5878@163.com>
2023-10-19 22:37:03 +0400

    添加prettire文件

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-}
+  }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
